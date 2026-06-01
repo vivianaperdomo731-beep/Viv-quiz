34e8f50ab60e9a1cf85d730afd966f689dd48789
vivianaperdomo731-beep <vivianaperdomo731@gmail.com>
2026-05-31 20:00:55 -0500

    Create main.jsx

---
+++ src/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ViviQuiz from './ViviQuiz'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ViviQuiz />
+  </React.StrictMode>
+)
